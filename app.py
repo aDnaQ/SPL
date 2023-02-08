b0638dfef7e20e0e42e4d839295f7eb081059f23
Adna Music <adna.music@bulme.at>
2023-02-08 16:24:41 +0100

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask.templating import render_template
+from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy as db
+
+from models import ParkplatzConfig
 
 
 app = Flask(__name__)
@@ -8,10 +11,16 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/parkraumueberwachung"
 
- 
+db = SQLAlchemy(app)
+
 @app.route("/")
 def run_app():
-    return render_template("base.html")
+
+    session: db.orm.scoping.scoped_session = db.session
+
+    parkplatzConfig = session.query(ParkplatzConfig).order_by(ParkplatzConfig.ParkplatzID).all()
+
+    return render_template("base.html",parkplatzConfig = parkplatzConfig)
  
  
 app.run()
